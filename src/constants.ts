d4a2477feb79ab03b53c416bea763e3dd9ab203b
larry-a4 <hushufeng@msn.com>
2025-04-28 01:35:43 +0800

    Recreate frontend repo

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+// Copyright (c), Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+export const DEVNET_PACKAGE_ID = '0xTODO';
+export const TESTNET_PACKAGE_ID =
+  '0xeb6262647895f5d1e270bd378cf54b2ca5aff15cb9dfe895485734490ebe07ee';
+export const MAINNET_PACKAGE_ID = '0xTODO';
